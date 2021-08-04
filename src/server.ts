c0815f3e4a7ba486221ba2c2b0549287d7c0f8ee
Gabriel <gabrielbarth98@gmail.com>
2021-08-03 23:49:26 -0400

    add cors to allow frontend requests

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import swaggerFile from './config/swagger.json';
 import './config/databaseConnection'
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use(cors());
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
